43ccffd2fbdcf536f9343181cc9d5e016b2d3708
bszaboshaw <bettina@Bettina-Szabo-Shaws-MacBook-Pro-17.local>
2014-02-27 10:59:04 +0000

    home content tweaks, WORK Index page
    3 column grid implemented on Work Index page, hover - white background
    and black border.

--- js/functions.js
+++ js/functions.js
@@ -21,4 +21,23 @@ $(document).ready(function() {
 
 
 
-  		
+ 	/* ---------------------------------------------------------------------- */
+	/*	Min-height
+	/* ---------------------------------------------------------------------- */
+
+	(function() {
+
+		// Set minimum height so footer will stay at the bottom of the window, even if there isn't enough content
+	// 	$('#content').css( 'min-height', $(window).outerHeight(true) - parseInt( $('body').css('border-top-width') ) - $('#header').outerHeight(true) - $('#footer').outerHeight(true) - $('#footer-bottom').outerHeight(true) + 11 );
+
+	// })();
+
+	$('#content').css( 'min-height', 
+		$(window).outerHeight(true) - $('#header').outerHeight(true) - $('#footer').outerHeight(true) - $('#footer-bottom').outerHeight(true) + 11 );
+
+	})();
+
+	/* end Min-height */
+
+
+});
--- js/main.js
+++ js/main.js
@@ -34,6 +34,26 @@
             scrollable: true
         });
 
+        /* ---------------------------------------------------------------------- */
+    /*  Min-height
+    /* ---------------------------------------------------------------------- */
+
+    (function() {
+
+        // Set minimum height so footer will stay at the bottom of the window, even if there isn't enough content
+    //  $('#content').css( 'min-height', $(window).outerHeight(true) - parseInt( $('body').css('border-top-width') ) - $('#header').outerHeight(true) - $('#footer').outerHeight(true) - $('#footer-bottom').outerHeight(true) + 11 );
+
+    // })();
+
+    $('#content').css( 'min-height', 
+        $(window).outerHeight(true) - $('header').outerHeight(true) - $('#footerWrapper').outerHeight(true) - $('#footer-bottom').outerHeight(true) - 167 );
+
+    })();
+
+    /* end Min-height */
+
+    
+
         // Shrink top bar size on scroll
         function checkPosition() {
             var targetOffset = $(".page-content").offset().top - 65,
